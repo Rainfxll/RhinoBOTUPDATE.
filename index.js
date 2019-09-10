2f2d03fe2e504199688433e21ff851ea3c077f1a
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 17:00:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -86,60 +86,35 @@ client.on('ready', () => {
 })
 
 
-/*
-The HELP command is used to display every command's name and description
-to the user, so that he may see what commands are available. The help
-command is also filtered by level, so if a user does not have access to
-a command, it is not shown to them. If a command name is given with the
-help command, its extended help is shown.
-*/
 
-exports.run = (client, message, args, level) => {
-    // If no specific command is called, show all filtered commands.
-    if (!args[0]) {
-      // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
-      const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
-  
-      // Here we have to get the command names only, and we use that array to get the longest name.
-      // This make the help commands "aligned" in the output.
-      const commandNames = myCommands.keyArray();
-      const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-  
-      let currentCategory = "";
-      let output = `= Command List =\n\n[Use ${message.settings.prefix}help <commandname> for details]\n`;
-      const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
-      sorted.forEach( c => {
-        const cat = c.help.category.toProperCase();
-        if (currentCategory !== cat) {
-          output += `\u200b\n== ${cat} ==\n`;
-          currentCategory = cat;
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "say",
+    aliases: ["bc", "broadcast"],
+    description: "Says your input via the bot",
+    usage: "<input>",
+    run: (client, message, args) => {
+        message.delete();
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
+
+        if (args.length < 0)
+            return message.reply("Nothing to say?").then(m => m.delete(5000));
+
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" : roleColorv);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
         }
-        output += `${message.settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
-      });
-      message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
-    } else {
-      // Show individual command's help.
-      let command = args[0];
-      if (client.commands.has(command)) {
-        command = client.commands.get(command);
-        if (level < client.levelCache[command.conf.permLevel]) return;
-        message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\naliases:: ${command.conf.aliases.join(", ")}\n= ${command.help.name} =`, {code:"asciidoc"});
-      }
     }
-  };
-  
-  exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ["h", "halp"],
-    permLevel: "User"
-  };
-  
-  exports.help = {
-    name: "help",
-    category: "System",
-    description: "Displays all the available commands for your permission level.",
-    usage: "help [command]"
-  };
+}
 
 client.login(process.env.token);
