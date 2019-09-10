dc7b1e4865158ecb8333063a1f5c788e6ac84f12
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 16:54:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -85,37 +85,61 @@ client.on('ready', () => {
 
 })
 
-// Stats
 
-const { version } = require("discord.js");
-const moment = require("moment");
-require("moment-duration-format");
+/*
+The HELP command is used to display every command's name and description
+to the user, so that he may see what commands are available. The help
+command is also filtered by level, so if a user does not have access to
+a command, it is not shown to them. If a command name is given with the
+help command, its extended help is shown.
+*/
 
-exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  message.channel.send(`= STATISTICS =
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User"
-};
-
-exports.help = {
-  name: "stats",
-  category: "Miscelaneous",
-  description: "Gives some useful bot statistics",
-  usage: "stats"
-};
-
+exports.run = (client, message, args, level) => {
+    // If no specific command is called, show all filtered commands.
+    if (!args[0]) {
+      // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
+      const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
+  
+      // Here we have to get the command names only, and we use that array to get the longest name.
+      // This make the help commands "aligned" in the output.
+      const commandNames = myCommands.keyArray();
+      const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+  
+      let currentCategory = "";
+      let output = `= Command List =\n\n[Use ${message.settings.prefix}help <commandname> for details]\n`;
+      const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+      sorted.forEach( c => {
+        const cat = c.help.category.toProperCase();
+        if (currentCategory !== cat) {
+          output += `\u200b\n== ${cat} ==\n`;
+          currentCategory = cat;
+        }
+        output += `${message.settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+      });
+      message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
+    } else {
+      // Show individual command's help.
+      let command = args[0];
+      if (client.commands.has(command)) {
+        command = client.commands.get(command);
+        if (level < client.levelCache[command.conf.permLevel]) return;
+        message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\naliases:: ${command.conf.aliases.join(", ")}\n= ${command.help.name} =`, {code:"asciidoc"});
+      }
+    }
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["h", "halp"],
+    permLevel: "User"
+  };
+  
+  exports.help = {
+    name: "help",
+    category: "System",
+    description: "Displays all the available commands for your permission level.",
+    usage: "help [command]"
+  };
 
 client.login(process.env.token);
