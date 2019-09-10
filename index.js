4b73287b4f6f6cdd57faab84930a654ed7e96879
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 16:37:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ client.on("guildMemberAdd", function(member){
 
 // Status
 
-let statuses = ['| Stabilny Hosting |','| Aktulizacja za 2 dni! |','| Aktywny już 24/7 |'];
+let statuses = ['| Stabilny Hosting |','| Aktulizacja za 2 dni! |','| Aktywny już 24/7 |',| 'Rhino BOT - v1.1 |'];
 
 client.on('ready', () => {
 
@@ -81,7 +81,7 @@ client.on('ready', () => {
         client.user.setPresence({ activity: { name: status }, status: 'online' });
 
 
-    }, 2000)
+    }, 5000)
 
 })
 
